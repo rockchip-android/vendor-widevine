e3e2480a1f4a4e3fc56086aee1648d7b68be0588
xzj <xzj@rock-chips.com>
2017-03-15 11:04:14 +0800

    porting widevine for 3126 7.1 from lly
    Change-Id: I6675896e01c1896a25f8732eb28b43dad501a0b2

--- widevine.mk
+++ widevine.mk
@@ -4,15 +4,10 @@ CUR_PATH := vendor/widevine
 ########################################################
 #add drm function
 #######################################################
-ifeq ($(strip $(TARGET_BOARD_PLATFORM)), rk3399)
-PRODUCT_COPY_FILES += vendor/widevine/lib/arm/rk3399/libdrmdecrypt.so:system/lib/libdrmdecrypt.so \
-                     vendor/widevine/lib/arm/rk3399/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
-                     vendor/widevine/lib/arm/rk3399/libwvm.so:system/vendor/lib/libwvm.so
-else
-PRODUCT_COPY_FILES += vendor/widevine/lib/arm/libdrmdecrypt.so:system/lib/libdrmdecrypt.so \
-                      vendor/widevine/lib64/arm/libdrmdecrypt.so:system/lib64/libdrmdecrypt.so \
-                      vendor/widevine/lib64/arm/libdrmwvmplugin.so:system/vendor/lib64/drm/libdrmwvmplugin.so \
-                      vendor/widevine/lib/arm/libwvm.so:system/vendor/lib/libwvm.so
+ifeq ($(strip $(TARGET_BOARD_PLATFORM)), rk312x)
+PRODUCT_COPY_FILES += vendor/widevine/lib/arm/rk3126/libdrmdecrypt.so:system/lib/libdrmdecrypt.so \
+                     vendor/widevine/lib/arm/rk3126/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
+                     vendor/widevine/lib/arm/rk3126/libwvm.so:system/vendor/lib/libwvm.so
 endif
 PRODUCT_COPY_FILES += vendor/widevine/lib/arm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
                      vendor/widevine/lib/arm/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
